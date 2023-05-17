25b64b636ac4b5d6d062eb708fc36a31face7453
StasSemak <semaks051@gmail.com>
2023-05-17 12:35:41 +0300

    add: images controller

---
+++ ASBack/Controllers/ImagesController.cs
@@ -0,0 +1,25 @@
+﻿using BussinesLogic.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASBack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult SaveCompressed([FromBody]string base64String)
+        {
+            try
+            {
+                var name = ImageWorker.SaveImage(base64String);
+                return Ok($"Image saved under name {name}");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
